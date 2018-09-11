b99eabafa2976dd8ce39503ac777037789a5bae1
Daniel Dopierała <dopierala.daniel@gmail.com>
2018-09-11 19:23:10 +0200

    Make game background totally black

--- game.cpp
+++ game.cpp
@@ -80,7 +80,7 @@ void Game::init() {
 	shader->use();
 
 	//Clear screen with black
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	//Turn on Z-buffer usage
 	glEnable(GL_DEPTH_TEST);
